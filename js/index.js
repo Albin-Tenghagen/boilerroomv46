12be288d2cbbc7fa6230f497340dff5b74cf2aad
ZanaPK0 <timezana10@gmail.com>
2024-11-15 09:29:38 +0100

    skapade array och objecttemplate

--- js/index.js
+++ js/index.js
@@ -1,16 +1,45 @@
 console.log("JavaScript file loaded correctly")
 
-let formNode = document.createElement("header")
+//----------------------------Form Creation------------------------------------------
+//*Created container for the form
+const formNode = document.createElement("header")
 document.body.appendChild(formNode)
 
-let formHead = document.createElement("h1")
+//*created h1 element and appended to formNode
+const formHead = document.createElement("h1")
 formNode.appendChild(formHead)
-formHead.innerHTML = "Anteckningsformulär"
+formHead.textContent = "Anteckningsformulär"
 
-let inputTitle = document.createElement("input")
+//*Created input element for assigning the objects title 
+const inputTitle = document.createElement("input")
 formNode.appendChild(inputTitle)
 inputTitle.setAttribute("id", "objectTitle")
 
-let inputDescription = document.createElement("input")
+//*Created input element for assigning the objects description
+const inputDescription = document.createElement("input")
 formNode.appendChild(inputDescription)
 inputDescription.setAttribute("id", "objectDescription")
+
+//* Created button element for saving the inputs and creating the Antecknings object
+const saveButton = document.createElement("button")
+formNode.appendChild(saveButton)
+saveButton.textContent = "Save note"
+saveButton.setAttribute("id", "objectSave")
+//---------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------
+//* Array for the objects
+let notesArray = [];
+//---------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------
+//* Object template
+let noteObjectTemplate = {
+    id: undefined,
+    title:  "",
+    description: "",
+    timeStamp: undefined 
+};
+//---------------------------------------------------------------------------------
+//  :kolon ;semikolon ,kommatecken .punkt 'tick' `Backtick` ´fronttick´ "Citattecken" ''
+
